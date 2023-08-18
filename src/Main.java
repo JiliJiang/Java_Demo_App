943f84316d13210333401791c68b0c2120c19242
jilijiang <jiangjili@gmail.com>
2023-08-18 16:21:18 -0700

    Java learning

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,17 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
+    public static int myNumber =1;
+    public static int GetNumber(int n){
+        return n+1;
+    }
+    public static int myNewNumber = GetNumber(myNumber);
+
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
+        System.out.println("Hello and welcome!");
+        System.out.println("my number is" + ": " + myNewNumber);
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
 
